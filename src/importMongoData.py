98a644c627a017e007232d074300e64d5635d9c2
Zellenon <Zellenon@gmail.com>
2021-12-07 15:31:09 -0600

    Added base MDB code, added ability col setup

--- src/importMongoData.py
+++ src/importMongoData.py
@@ -1,3 +1,41 @@
 """Seperate program. Imports champion & move database into Mongo."""
 
-print("Goodbye World!")
+import sys
+import json
+import pandas as pd
+from pymongo import MongoClient
+
+
+def connect():
+    """Return a connection to the database."""
+    try:
+        client = MongoClient("127.0.0.1")
+        db = client.CDBFP
+        print("Connected succesfully.")
+        return db
+    except Exception as e:
+        print("Could not connect to server.")
+        print(e)
+        sys.exit(-1)
+
+
+def getData(fpath):
+    """Import a csv file at path csv_name to a mongo colection.
+
+    returns: count of the documants in the new collection
+    """
+    data = open(fpath).readlines()
+    data = [w.replace("\n", "") for w in data]
+    data = [w.split(',\t') for w in data]
+    headers = data[0]
+    headers[0] = "index"
+    entries = [{headers[c]: w[c] for c in range(0, len(headers))}
+               for w in data[1:]]
+    return entries
+
+
+db = connect()
+db.abilities.drop()
+db.create_collection("abilities")
+entries = getData("../res/abilities.tsv")
+db.abilities.insert_many(entries)
